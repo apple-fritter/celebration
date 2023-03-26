0b2385b444c476e71b02730d96bc7a888b88a9c3
@apple-fritter <125221355+apple-fritter@users.noreply.github.com>
2023-03-26 16:03:17 -0700

    Update update_installed_packages.sh

--- root/update_installed_packages.sh
+++ root/update_installed_packages.sh
@@ -1,1 +1,9 @@
+# The command apt-get update -y && apt-get dist-upgrade -y is used in Linux to update the system and upgrade all packages to the latest available versions.
+
+# The apt-get update command downloads the package lists from the repositories and updates them to the latest version. The -y flag is used to automatically answer "yes" to any prompts that may come up during the update process.
+
+# The dist-upgrade command performs a more aggressive upgrade than the upgrade command, which only upgrades packages that have new versions available. dist-upgrade can also remove packages that are no longer needed or cause conflicts. The -y flag is used here to automatically answer "yes" to any prompts during the upgrade process.
+
+# It's important to note that running this command can potentially cause issues with your system, especially if you have made custom changes or have specific dependencies. Therefore, it's recommended to backup your data and make sure you understand the implications of the upgrade before running this command. It's also a good practice to periodically update and upgrade your system to keep it secure and up-to-date.
+
 apt-get update -y && apt-get dist-upgrade -y
